e362649df296d658534c9fbd2bae9064092d3f21
gilbertoCanizales <90221459+gilbertoCanizales@users.noreply.github.com>
2021-09-29 20:40:59 -0500

    Delete server/base de datos directory

--- server/base de datos/Crud Genero.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM larockola.genero;
-INSERT INTO larockola.genero (idGenero , descripcionGenero ) VALUES ("2","Rock en Español");
-DELETE FROM larockola.genero WHERE idGenero = 2 ;
-UPDATE larockola.genero SET descripcionGenero = "Rock en Español" WHERE idGenero = 1;
--- server/base de datos/Crud ListaReproduccion.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM larockola.listareproduccion;
-INSERT INTO larockola.listareproduccion (idListaReproduccionlistacancion, idUsuarioLista, fechaUltimaReproduccion, listaFavorita ) VALUES ("2","2","2021-09-27","Favorito");
-DELETE FROM larockola.listareproduccion WHERE idListaReproduccion = 2 ;
-UPDATE larockola.listareproduccion SET idUsuarioLista = "1",fechaUltimaReproduccion = "2021-09-26",listaFavorita = "Favorito" WHERE idListaReproduccion = 1;
